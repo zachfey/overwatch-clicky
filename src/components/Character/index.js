1bb7fb2fa969ae185f6075ef855c01e678372bee
Zach <zachary.fey@gmail.com>
2019-06-22 00:52:28 -0400

    filled out characters.json

--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -6,7 +6,7 @@ function Character(props) {
     return (
 
         <div className="card">
-            <div className="img-container">
+            <div className="card-img-top img-container">
                 {props.character.map(item => (
                     <img alt={item.name} src={item.image} />
                 ))}
