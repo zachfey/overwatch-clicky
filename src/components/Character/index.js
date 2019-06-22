39813ca90e3b0c324237f0dd00924d4448b7d563
Zach <zachary.fey@gmail.com>
2019-06-22 00:44:35 -0400

    got characters displaying on page, created characters.json

---
+++ src/components/Character/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+
+function Character(props) {
+    console.log(props)
+    return (
+
+        <div className="card">
+            <div className="img-container">
+                {props.character.map(item => (
+                    <img alt={item.name} src={item.image} />
+                ))}
+                {/* onClick={() => props.guessCharacter(props.id)} */}
+
+            </div>
+        </div>
+    );
+}
+
+export default Character;
