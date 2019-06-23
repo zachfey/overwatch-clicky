2591036b001350901864b071474c782ad6d7b162
Zach <zachary.fey@gmail.com>
2019-06-22 22:09:29 -0400

    got onclick functionality mostly working - will not support multiple clicks

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
-import React from 'react';
-import character from './characters.json';
+import React, { Component } from 'react';
+import characters from './characters.json';
 import Character from './components/Character'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Clicky Game
+class App extends Component {
+
+
+  // Setting this.state.friends to the friends json array
+  state = {
+    characters
+  };
+
+  guessCharacter = id => {
+    // Filter this.state.friends for friends with an id not equal to the id being removed
+    console.log('before')
+    console.log(this.state.characters)
+    console.log(id)
+    console.log('now change....')
+    let characters = this.state.characters.forEach(character => {
+      if (character.id === id) {
+        character.guessed = true;
+        console.log(character)
+      }
+    });
+    // console.log(characters)
+    this.setState({ characters });
+  };
+  render() {
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          Clicky Game
       </header>
-      <Character
-        character = {character}
-      />
-    </div>
-  );
+        <Character
+          character={characters}
+          guessCharacter={this.guessCharacter}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Character/index.js
+++ src/components/Character/index.js
@@ -2,15 +2,13 @@ import React from "react";
 import "./style.css";
 
 function Character(props) {
-    console.log(props)
     return (
 
-        <div className="card">
+        <div className="card" >
             <div className="card-img-top img-container">
                 {props.character.map(item => (
-                    <img alt={item.name} src={item.image} />
+                    <img alt={item.name} src={item.image} onClick={() => props.guessCharacter(item.id)} data-guessed ={item.guessed}/>
                 ))}
-                {/* onClick={() => props.guessCharacter(props.id)} */}
 
             </div>
         </div>
