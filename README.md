cdfe4343605428d123049ad231d3677a466e060a
zachfey <zachary.fey@gmail.com>
2019-07-23 21:12:39 -0400

    Updates

--- README.md
+++
@@ -1,7 +0,0 @@
-## Overwatch Memory Game
-
-Click to guess a character and increase your score. But be careful, if you guess the same character twice, your score resets to 0!
-
-Check it out here: https://zachfey.github.io/overwatch-clicky/
-
-This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
---
+++ precache-manifest.adac47b8da75b0768458ed8755f036c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6081df408cc893535179283dd9004630",
+    "url": "/overwatch-clicky/index.html"
+  },
+  {
+    "revision": "b3e1af7e62719ad99de5",
+    "url": "/overwatch-clicky/static/css/main.8212b4d9.chunk.css"
+  },
+  {
+    "revision": "5b8bbdbb75b8b44c442b",
+    "url": "/overwatch-clicky/static/js/2.6fcf23cc.chunk.js"
+  },
+  {
+    "revision": "b3e1af7e62719ad99de5",
+    "url": "/overwatch-clicky/static/js/main.5e4b69dd.chunk.js"
+  },
+  {
+    "revision": "50bf98298ce23e49506d",
+    "url": "/overwatch-clicky/static/js/runtime~main.6533900c.js"
+  }
+]);
